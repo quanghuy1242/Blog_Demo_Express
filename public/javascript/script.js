2d1748bef9c94d83a7eeedc58db59136a0be768c
quanghuy1242 <katygaga124@gmail.com>
2019-04-17 00:39:07 +0700

    changed: scroll simultaneously

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -5,4 +5,29 @@ $(document).ready(function () {
   $('#content').keyup(function () {
     $('#preview').html(md.render($('#content').val()));
   })
+
+  
+  let isLeftScrollTop = false;
+  let isRightScrollTop = false;
+  let contentHTML = document.querySelector('#content');
+  let previewHTML = document.querySelector('#preview');
+  $('#content').scroll(function(e) {
+    if (isRightScrollTop) {
+      return isRightScrollTop = false;
+    }
+    let percent = this.scrollTop / (this.scrollHeight - this.clientHeight);
+    let a = previewHTML.scrollHeight - previewHTML.clientHeight;
+    $('#preview').scrollTop(percent * a);
+    isLeftScrollTop = true;
+  });
+
+  $('#preview').scroll(function(e) {
+    if (isLeftScrollTop) {
+      return isLeftScrollTop = false;
+    }
+    let percent = this.scrollTop / (this.scrollHeight - this.clientHeight);
+    let a = contentHTML.scrollHeight - contentHTML.clientHeight;
+    $('#content').scrollTop(percent * a);
+    isRightScrollTop = true;
+  });
 })
