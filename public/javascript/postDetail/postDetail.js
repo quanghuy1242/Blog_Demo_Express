d582f8bb3619f5114822d48755738364f5c72f41
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-20 11:56:34 +0700

    added list of post template

--- public/javascript/postDetail/postDetail.js
+++ public/javascript/postDetail/postDetail.js
@@ -1,1 +1,10 @@
 var tagsChipSet = new MDCChipSet(document.querySelector('.mdc-chip-set'));
+
+var dialogListPostHTML = document.querySelector('#dialog-list-post');
+var dialogListPost = new MDCDialog(dialogListPostHTML);
+dialogListPost.scrimClickAction = '';
+dialogListPost.escapeKeyAction = '';
+
+document.querySelector('#btn-list-post').addEventListener('click', function() {
+  dialogListPost.open();
+});
